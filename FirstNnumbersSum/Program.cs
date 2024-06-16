c9fc88022c97e852b2d490b28eff848329c13782
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-16 20:42:20 +0300

    Initial commit

---
+++ FirstNnumbersSum/Program.cs
@@ -0,0 +1,25 @@
+﻿class FirstNnumbersSum
+{
+    public static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int sum = 0;
+        string expression = "";
+        
+        for (int i = 1; i <= n; i ++ ) 
+        {
+           
+            sum +=i;
+            if(i==n)
+            {
+                expression += $"{i}=";
+            }
+            else
+            {
+                expression += $"{i}+";
+            }
+
+        }
+        Console.WriteLine($"{expression}{sum}");
+    }
+}
